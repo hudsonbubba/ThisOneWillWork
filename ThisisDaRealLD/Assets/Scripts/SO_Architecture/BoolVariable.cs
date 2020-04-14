8ab4977474103475afcaa9bd994192186893589c
hudsonbubba <42046042+hudsonbubba@users.noreply.github.com>
2020-04-14 14:26:24 -0400

    BoilerplateUpdate
    - Added Doodle Studio
    - Added John Gilcrest Code

---
+++ ThisisDaRealLD/Assets/Scripts/SO_Architecture/BoolVariable.cs
@@ -0,0 +1,25 @@
+﻿// ----------------------------------------------------------------------------
+// Unite 2017 - Game Architecture with Scriptable Objects
+// 
+// Author: Ryan Hipple
+// Date:   10/04/17
+// ----------------------------------------------------------------------------
+
+using UnityEngine;
+
+
+    [CreateAssetMenu]
+    public class BoolVariable : ScriptableObject
+    {
+#if UNITY_EDITOR
+        [Multiline]
+        public string DeveloperDescription = "";
+#endif
+        public bool Value;
+
+        public void SetValue(bool value)
+        {
+            Value = value;
+        }
+
+    }
