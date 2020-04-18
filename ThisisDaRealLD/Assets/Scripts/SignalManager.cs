276702c0501150422dca0e4d522f84f9467c1548
JeeroyLenkinz <rdotsikas@gmail.com>
2020-04-18 19:00:52 -0400

    Signal Manager added
    - Gain signal per turn and when relay passed, script listens for these events and updates accordingly.
    - Event created for relay passed (not thrown by anyone yet)
    - Signal SO float variable created
    - Signal Manager prefab

---
+++ ThisisDaRealLD/Assets/Scripts/SignalManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignalManager : MonoBehaviour
+{
+    public FloatVariable signalSO;
+    public float SIGNAL_MAX;
+    public float signalGainPerTurn;
+    public float signalGainPerRelay;
+
+    public void e_endOfTurn()
+    {
+        signalSO.Value += signalGainPerTurn;
+        if (signalSO.Value >= SIGNAL_MAX)
+        {
+            signalSO.Value = SIGNAL_MAX;
+            signalComplete();
+        }
+    }
+
+    public void e_signalRelayPassed()
+    {
+        signalSO.Value += signalGainPerRelay;
+        if (signalSO.Value >= SIGNAL_MAX)
+        {
+            signalSO.Value = SIGNAL_MAX;
+            signalComplete();
+        }
+    }
+
+    void signalComplete()
+    {
+        Debug.Log("Signal has reached the maximum. Reach max speed to get outta there!");
+    }
+}
