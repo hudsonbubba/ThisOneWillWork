b91a83ecba3267ea124ceda632d8ac42e9bc8027
hudsonbubba <42046042+hudsonbubba@users.noreply.github.com>
2020-04-19 09:28:40 -0400

    VFX_Fade

---
+++ ThisisDaRealLD/Assets/Scripts/Shader/Dissolve.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissolve : MonoBehaviour
+{
+
+    Material mat;
+
+    bool isDissolving = false;
+    float fade = 1f;
+
+    private void Start()
+    {
+        mat = GetComponent<SpriteRenderer>().material;
+    }
+
+    private void Update()
+    {
+        if (isDissolving)
+        {
+            fade -= Time.deltaTime;
+
+            if (fade <= 0f)
+            {
+                fade = 0f;
+                isDissolving = false;
+            }
+
+            mat.SetFloat("_Fade", fade);
+        }
+    }
+
+    public void e_FadeOut()
+    {
+        isDissolving = true;
+    }
+
+}
