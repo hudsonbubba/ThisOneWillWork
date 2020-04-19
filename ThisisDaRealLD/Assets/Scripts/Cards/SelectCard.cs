348de3798d4e4a016165a5cf326f0d62442d41be
JeeroyLenkinz <rdotsikas@gmail.com>
2020-04-19 14:22:40 -0400

    Select action implemented

---
+++ ThisisDaRealLD/Assets/Scripts/Cards/SelectCard.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectCard : MonoBehaviour
+{
+    public IntegerVariable playedCard;
+    public GameEvent resetSelectionEvent;
+
+    private void Start()
+    {
+        e_resetSelection();
+    }
+
+    public void e_selectCard(int handIndex)
+    {
+        resetSelectionEvent.Raise();
+        playedCard.SetValue(handIndex);
+        this.gameObject.GetComponent<Image>().color = Color.blue;
+    }
+
+    public void e_resetSelection()
+    {
+        this.gameObject.GetComponent<Image>().color = Color.white;
+    }
+}
