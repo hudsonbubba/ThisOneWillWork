f62c62ac4181f15838ff05a24708f51b16d7ff9d
JeeroyLenkinz <rdotsikas@gmail.com>
2020-04-19 11:51:17 -0400

    Bug fixes and tweaks to energy and signal managers

--- ThisisDaRealLD/Assets/Scripts/EnergyManager.cs
+++ ThisisDaRealLD/Assets/Scripts/EnergyManager.cs
@@ -7,20 +7,38 @@ public class EnergyManager : MonoBehaviour
     public FloatVariable energySO;
     public float energyLossPerTurn;
     public float energyGainPerKill;
-    public float energyGainPerDiscard;
-    
+    public float energyGainPerBurn;
+    public float energyMax;
+
+    private void Start()
+    {
+        energySO.SetValue(energyMax);
+    }
+
     public void e_endOfTurn()
     {
         energySO.Value -= energyLossPerTurn;
+        if (energySO.Value <= 0)
+        {
+            Debug.Log("Energy fully depleted. Game Over!");
+        }
     }
 
     public void e_enemyDestroyed()
     {
         energySO.Value += energyGainPerKill;
+        if (energySO.Value >= energyMax)
+        {
+            energySO.SetValue(energyMax);
+        }
     }
 
-    public void e_cardDiscarded()
+    public void e_cardBurned()
     {
-        energySO.Value += energyGainPerDiscard;
+        energySO.Value += energyGainPerBurn;
+        if (energySO.Value >= energyMax)
+        {
+            energySO.SetValue(energyMax);
+        }
     }
 }
