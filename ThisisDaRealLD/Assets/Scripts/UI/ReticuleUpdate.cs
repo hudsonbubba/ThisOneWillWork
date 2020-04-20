b28e810cadc9e9f0f8d00710fca271ee62dbdc7a
hudsonbubba <42046042+hudsonbubba@users.noreply.github.com>
2020-04-19 22:40:23 -0400

    ReticuleUpdate

---
+++ ThisisDaRealLD/Assets/Scripts/UI/ReticuleUpdate.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReticuleUpdate : MonoBehaviour
+{
+
+    public BoardState telegraphedBoardState;
+    SpriteRenderer spriteRenderer;
+    public Sprite playerReticule;
+    public Sprite enemyReticule;
+    string myShipString;
+
+    private void Start()
+    {
+        myShipString = GetComponent<MouseOverReticule>().myShipString;
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+
+        if(myShipString == "p")
+        {
+            spriteRenderer.sprite = playerReticule;
+        }
+        else
+        {
+            spriteRenderer.sprite = enemyReticule;
+        }
+    }
+
+    public void e_updatePosition()
+    {
+        for (int row = 1; row < (telegraphedBoardState.board.GetLength(0) - 1); row++)
+        {
+            for (int column = 0; column < telegraphedBoardState.board.GetLength(1); column++)
+            {
+                string indexValue = telegraphedBoardState.board[row, column];
+                if (string.Equals(indexValue, myShipString))
+                {
+                    transform.position = new Vector3(column * 2.4f, row * -1.73f, 0f); //THIS IS HOW IT SHOULD BE
+                }
+             
+            }
+        }
+    }
+}
