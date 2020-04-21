d6a4d2fba82e09b29ce08f8390e3f2d7e9f17e9e
hudsonbubba <42046042+hudsonbubba@users.noreply.github.com>
2020-04-20 20:06:21 -0400

    Intro

---
+++ ThisisDaRealLD/Assets/Scripts/UI/Intro/IntroAnim.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroAnim : MonoBehaviour
+{
+
+    Animator animator;
+    AudioSource audio;
+
+    bool isAnim = true;
+    //AudioClip beepBoop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0) && !isAnim)
+        {
+            animator.SetTrigger("Advance");
+            audio.Play();
+        }
+
+    }
+
+    public void ae_soundCodec()
+    {
+        audio.Play();
+    }
+
+    public void ae_nextSound()
+    {
+        //audio.clip = beepBoop;
+        audio.Play();
+    }
+
+    public void ae_endScene()
+    {
+        //endScene
+    }
+
+    public void ae_doneAnim()
+    {
+        isAnim = false;
+    }
+}
